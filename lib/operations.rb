6bea8bcc0bb5c49fb951b698b38ad9cd5d9c7a6e
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 17:30:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,12 +10,12 @@ end
 
 
 
-def not_safe?(speed)
-	if speed >= 60 ? true : false
-  elsif speed <=40 true : false
-  else speed > 40
-  end
-end
+# def not_safe?(speed)
+# 	if speed >= 60 ? true : false
+#   elsif speed <=40 true : false
+#   else speed > 40
+#   end
+# end
 	
 
 
